f48df05c141c1041b806c850d7ba13467abb30c5
julian-pulpo <julian.schrodinger@gmail.com>
2021-06-26 22:59:33 -0500

    estos son mis ejemplos de codigo no boqueante

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+fs.readFile('./text.txt', function(err, data) {
+    if (err) {
+        console.log(err);
+    }
+    if (data) {
+        console.log(data.toString());
+    }
+});
