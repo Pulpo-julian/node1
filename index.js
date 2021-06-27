dcf5d1bc77ddf915b15bba565334f91aa82caf0f
julian-pulpo <julian.schrodinger@gmail.com>
2021-06-27 00:44:55 -0500

    añado partes de la peticion del servidor

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
-http.createServer(function (req, res) {
+const handleServer = http.createServer(function (req, res) {
     res.writeHead(200)
     res.write("<h1>hola </h1>");
     res.end();
-}).listen(3000);
+});
+
+const server = http.createServer(handleServer);
+
+server.listen(3000, function () {
+    console.log
+});
